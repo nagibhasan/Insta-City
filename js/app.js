71e96e450a5c385019fb510681b2e75397c62cab
nagibhasan <nagibhasan132@gmail.com>
2023-03-12 20:15:35 +0600

    post commit

--- js/app.js
+++ js/app.js
@@ -55,7 +55,6 @@ const createPost = (post) => {
     const userImage = post.userImage;
     const div = document.createElement( "article" );
     div.classList.add( "post" );
-    console.log(post);
     div.innerHTML = `
               <div class="post__header">
                 <div class="post__profile">
@@ -122,9 +121,9 @@ const createPost = (post) => {
                   <div class="post__description">
                     <small>
                       <a class="post__name--underline" href="#">
-                          ${post.comments?.user}
+                          ${post.comments[0]?.user}
                       </a>
-                      ${post.comments?.text}
+                      ${post.comments[0]?.text}
                     </small>
                   </div>
                   <span class="post__date-time">30 minutes ago</span>
